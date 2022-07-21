1c45a25bd2fa6bd152f6b6058eee9783ab16ed83
Yas Wood <yaswood@Yass-MacBook-Air.local>
2022-07-21 14:42:17 -0400

    adding group repo

--- code-connected-server/src/main/java/com/gate6coders/codeconnectedserver/domain/group/repo/GroupRepo.java
+++ code-connected-server/src/main/java/com/gate6coders/codeconnectedserver/domain/group/repo/GroupRepo.java
@@ -1,8 +1,11 @@
 package com.gate6coders.codeconnectedserver.domain.group.repo;
 
 import com.gate6coders.codeconnectedserver.domain.group.model.Group;
+import com.gate6coders.codeconnectedserver.domain.profile.model.Profile;
 
 import java.util.Optional;
 
 public interface GroupRepo {
+    Optional<Group> findById(Long id);
+    Optional<Group> findByGroupName(String groupName);
 }
