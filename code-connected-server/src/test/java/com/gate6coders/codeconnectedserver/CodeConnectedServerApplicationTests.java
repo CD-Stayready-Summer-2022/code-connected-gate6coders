8f4e01346c361ed15d54faaf318243b2eeb7b073
Yas Wood <yaswood@Yass-MacBook-Air.local>
2022-07-21 10:56:47 -0400

    starting proj

---
+++ code-connected-server/src/test/java/com/gate6coders/codeconnectedserver/CodeConnectedServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gate6coders.codeconnectedserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeConnectedServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
