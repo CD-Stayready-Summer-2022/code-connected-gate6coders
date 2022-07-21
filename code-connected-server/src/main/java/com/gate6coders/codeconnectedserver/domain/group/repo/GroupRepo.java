ae78a266606e461f6824864184bcfd9c7eea1aaa
Tory Doby <torydoby43@gmail.com>
2022-07-21 14:39:01 -0400

    m

--- code-connected-server/src/main/java/com/gate6coders/codeconnectedserver/domain/group/repo/GroupRepo.java
+++ code-connected-server/src/main/java/com/gate6coders/codeconnectedserver/domain/group/repo/GroupRepo.java
@@ -1,7 +1,8 @@
 package com.gate6coders.codeconnectedserver.domain.group.repo;
 
-import net.javaguides.springboot.springbootbackend.Model.Employee;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-public interface GroupRepo extends JpaRepossitory<Group, >{
+public interface GroupRepo extends JpaRepossitory<Group, id> {
+
 }
