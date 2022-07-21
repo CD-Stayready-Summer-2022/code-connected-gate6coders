3f5f7c723164c227bdd584c0800c11665eb404ec
AbdullahAl-rubaye <93665223+AbdullahAl-rubaye@users.noreply.github.com>
2022-07-21 14:09:41 -0400

    message

--- code-connected-server/src/main/java/com/gate6coders/codeconnectedserver/domain/post/model/Post.java
+++ code-connected-server/src/main/java/com/gate6coders/codeconnectedserver/domain/post/model/Post.java
@@ -1,4 +1,36 @@
 package com.gate6coders.codeconnectedserver.domain.post.model;
 
+import com.gate6coders.codeconnectedserver.domain.comment.model.Comment;
+import com.gate6coders.codeconnectedserver.domain.profile.model.Profile;
+import org.springframework.lang.NonNull;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.List;
+import java.util.Set;
+
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NonNull
+    private Profile creator;
+    @NonNull
+    private String postContent;
+    @NonNull
+    private Set<Profile> likes;
+    @NonNull
+    private List<Comment> comments;
+
+
+    public Post(Long id, @NonNull Profile creator, @NonNull String postContent, @NonNull Set<Profile> likes, @NonNull List<Comment> comments) {
+        this.id = id;
+        this.creator = creator;
+        this.postContent = postContent;
+        this.likes = likes;
+        this.comments = comments;
+    }
 }
+
+
