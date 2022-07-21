79597cfa4a74ea66aff81e87c16aee9182196a9d
Khalil Brewington <kbrewing02@gmail.com>
2022-07-21 14:33:45 -0400

    updates

--- code-connected-server/src/main/java/com/gate6coders/codeconnectedserver/domain/post/model/Post.java
+++ code-connected-server/src/main/java/com/gate6coders/codeconnectedserver/domain/post/model/Post.java
@@ -1,4 +1,38 @@
 package com.gate6coders.codeconnectedserver.domain.post.model;
 
+import com.gate6coders.codeconnectedserver.domain.comment.model.Comment;
+import com.gate6coders.codeconnectedserver.domain.profile.model.Profile;
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.List;
+import java.util.Set;
+
+@Data
+@Entity
+@ToString
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NonNull
+    private Profile creator;
+
+    @NonNull
+    private String postContent;
+
+    @NonNull
+    private Set<Profile> likes;
+
+    @NonNull
+    private List<Comment> comments;
+
 }
+
