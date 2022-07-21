7e4dd9126bf049191c8e81c9af5d026d5e906a21
AbdullahAl-rubaye <93665223+AbdullahAl-rubaye@users.noreply.github.com>
2022-07-21 17:17:50 -0400

    aa

--- code-connected-server/src/main/java/com/gate6coders/codeconnectedserver/domain/post/service/PostService.java
+++ code-connected-server/src/main/java/com/gate6coders/codeconnectedserver/domain/post/service/PostService.java
@@ -1,7 +1,16 @@
 package com.gate6coders.codeconnectedserver.domain.post.service;
 
+import com.gate6coders.codeconnectedserver.domain.core.exceptions.ProfileNotFoundException;
+import com.gate6coders.codeconnectedserver.domain.post.model.Post;
 import com.gate6coders.codeconnectedserver.domain.profile.model.Profile;
 
-public interface PostService {
+import java.util.List;
 
+public interface PostService {
+    Profile getById(Long id) throws ProfileNotFoundException;
+    List<Profile> getProfiles();
+    List<Profile> getFollowers();
+    Long getById(Post post);
+    Integer getNumberOfLikes();
+    Post findByFullName();
 }
--- code-connected-server/src/main/java/com/gate6coders/codeconnectedserver/domain/post/service/PostServiceImpl.java
+++ code-connected-server/src/main/java/com/gate6coders/codeconnectedserver/domain/post/service/PostServiceImpl.java
@@ -1,4 +1,40 @@
 package com.gate6coders.codeconnectedserver.domain.post.service;
 
-public class PostServiceImpl {
+import com.gate6coders.codeconnectedserver.domain.core.exceptions.ProfileNotFoundException;
+import com.gate6coders.codeconnectedserver.domain.post.model.Post;
+import com.gate6coders.codeconnectedserver.domain.profile.model.Profile;
+
+import java.util.List;
+
+public class PostServiceImpl implements PostService {
+
+    @Override
+    public Profile getById(Long id) throws ProfileNotFoundException {
+        return null;
+    }
+
+    @Override
+    public List<Profile> getProfiles() {
+        return null;
+    }
+
+    @Override
+    public List<Profile> getFollowers() {
+        return null;
+    }
+
+    @Override
+    public Long getById(Post post) {
+        return null;
+    }
+
+    @Override
+    public Integer getNumberOfLikes() {
+        return null;
+    }
+
+    @Override
+    public Post findByFullName() {
+        return null;
+    }
 }
