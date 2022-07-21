27d91d365f1de20b662b6b7f8c4717ba6f49f287
AbdullahAl-rubaye <93665223+AbdullahAl-rubaye@users.noreply.github.com>
2022-07-21 15:09:21 -0400

    new

--- code-connected-server/src/main/java/com/gate6coders/codeconnectedserver/domain/post/model/Post.java
+++ code-connected-server/src/main/java/com/gate6coders/codeconnectedserver/domain/post/model/Post.java
@@ -2,21 +2,16 @@ package com.gate6coders.codeconnectedserver.domain.post.model;
 
 import com.gate6coders.codeconnectedserver.domain.comment.model.Comment;
 import com.gate6coders.codeconnectedserver.domain.profile.model.Profile;
-<<<<<<< HEAD
-import org.springframework.lang.NonNull;
 
-=======
 import lombok.*;
 
 import javax.persistence.Entity;
->>>>>>> b8cc4cd1ab9196d82d0e102f86b93c30889d3a35
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.util.List;
 import java.util.Set;
 
-<<<<<<< HEAD
 public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -41,30 +36,4 @@ public class Post {
 }
 
 
-=======
-@Data
-@Entity
-@ToString
-@NoArgsConstructor
-@RequiredArgsConstructor
-public class Post {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @NonNull
-    private Profile creator;
-
-    @NonNull
-    private String postContent;
-
-    @NonNull
-    private Set<Profile> likes;
-
-    @NonNull
-    private List<Comment> comments;
-
-}
-
->>>>>>> b8cc4cd1ab9196d82d0e102f86b93c30889d3a35
